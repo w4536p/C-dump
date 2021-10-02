2a5aa37d1b8bd433a24eca75284f03859e6db2f2
woop-3533 <81119838+woop-3533@users.noreply.github.com>
2021-10-02 15:36:15 +0000

    Update text.c

--- source/text.c
+++ source/text.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
+
 int main() {
 
-	
 	char test[100]="\0";
+	
 	printf("type anything: ");
 	scanf("%s",test);
-
 	printf("%s",test);
-
 	printf(" this was added on badly!");
-
 	printf(" this was too.");
-
 	printf("\n");
 }
